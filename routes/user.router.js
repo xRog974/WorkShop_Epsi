ac0e1d47e524da705e316c14ac0ca58c60652c20
thomasboidun <thomas.boidun@gmail.com>
2022-10-11 16:36:40 +0200

    Add of users/logOut route

--- routes/user.router.js
+++ routes/user.router.js
@@ -5,6 +5,7 @@ const UserController = require('../controllers/user.controller');
 
 router.post('/signUp', UserController.createUser);
 router.post('/signIn', UserController.login);
+router.post('/logOut', UserController.logOut);
 router.get('/getAll', UserController.getAllUsers);
 router.get('/getbyId', UserController.getUserById);
 router.put('/updatebyId', UserController.updateUserById);
